ac5dec4bf1b9d3c248d7a7b1c9972acc361299ba
Tate Braeckel <90567271+tbraeck@users.noreply.github.com>
2024-12-07 16:42:54 -0700

    adding data.json and letter templates

--- app/components/LetterEditor.js
+++ app/components/LetterEditor.js
@@ -1,26 +1,54 @@
-// components/LetterEditor.js
+// components/LetterEditor.js  
 import { useState } from "react";
-
-const LetterEditor = ({ onSend }) => {
-  const [content, setContent] = useState("");
-
-  return (
-    <div className=" mt-5 flex flex-col justify-center items-center border rounded-md border-4 border-gray-400 bg-gray-400">
-      <div className="  ">
-          <textarea
-          rows={10} // Controls the height by rows
-          maxLength={500} // Limits the character count to 500
-          value={content}
-          onChange={(e) => setContent(e.target.value)}
-          className="resize-none h-48 w-96 p-2  "
-          placeholder="Write your letter here..."
-        />
-      </div>
-      <div >
-        <button onClick={() => onSend(content)} className="justify-center bg-violet-400  p-1 px-2 mt-4 mb-4 text-white drop-shadow-sm border rounded-md ">Send Letter</button>  
-      </div>
-    </div>
-  );
-};
-
+import templates from '../data.json';
+  
+const LetterEditor = ({ onSend }) => {  
+  const [content, setContent] = useState("");  
+  const [selectedTemplate, setSelectedTemplate] = useState(null);  
+  
+  const handleTemplateChange = (event) => {  
+   const selectedTemplateId = event.target.value;  
+   const template = templates.find((template) => template.id === parseInt(selectedTemplateId));  
+   setSelectedTemplate(template);  
+   setContent(template.body);  
+  };  
+  
+  const handleContentChange = (event) => {  
+   setContent(event.target.value);  
+  };  
+  
+  return (  
+   <div className=" mt-5 flex flex-col justify-center items-center border rounded-md border-4 border-gray-400 bg-gray-400">  
+    <div className="mb-4">  
+      <h2>Choose a template:</h2>  
+      {templates.map((template) => (  
+       <div key={template.id}>  
+        <input  
+          type="radio"  
+          id={template.id}  
+          name="template"  
+          value={template.id}  
+          onChange={handleTemplateChange}  
+        />  
+        <label htmlFor={template.id}>{template.name}</label>  
+       </div>  
+      ))}  
+    </div>  
+    <div>  
+      <textarea  
+       rows={10} // Controls the height by rows  
+       maxLength={500} // Limits the character count to 500  
+       value={content}  
+       onChange={handleContentChange}  
+       className="resize-none h-48 w-96 p-2"  
+       placeholder="Write your letter here..."  
+      />  
+    </div>  
+    <div>  
+      <button onClick={() => onSend(content)} className="justify-center bg-violet-400 p-1 px-2 mt-4 mb-4 text-white drop-shadow-sm border rounded-md">Send Letter</button>  
+    </div>  
+   </div>  
+  );  
+};  
+  
 export default LetterEditor;
